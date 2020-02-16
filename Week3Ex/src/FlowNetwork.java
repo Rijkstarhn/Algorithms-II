cd0d38251f5524d08f5468f6c3f3b62062a6bbc6
Rijkstarhn <32129291+Rijkstarhn@users.noreply.github.com>
2020-02-16 12:25:53 +0800

    FlowEdge

---
+++ Week3Ex/src/FlowNetwork.java
@@ -0,0 +1,30 @@
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+
+public class FlowNetwork {
+    
+    private int V;
+    private Bag<FlowEdge>[] adj;
+    
+    public FlowNetwork(int v) {
+        V = v;
+        adj = (Bag<FlowEdge>[])new Bag[V];
+        for (int i = 0; i < V; i++) {
+            adj[i] = new Bag<FlowEdge>();
+        }
+    }
+    
+    public FlowNetwork(In in) {
+        
+    }
+    
+    public void addEdge(FlowEdge e) {
+        int v = e.from(), w = e.to();
+        adj[v].add(e);
+        adj[w].add(e);
+    }
+    
+    Iterable<FlowEdge> adj(int v) {
+        return adj[v];
+    }
+}
